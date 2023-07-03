8e200306a9652fbb94269c12147cabe91ebd3b94
Susumu Yagi <susumu.yagi@gmail.com>
2023-07-03 16:44:13 +0900

    init

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME=fractol
+CFLAGS=-Wall -Wextra -Werror
+SRC_DIR=.
+SRCS=src/main.c
+OBJS=$(SRCS:.c=.o)
+MINILIBX_DIR=./minilibx_opengl_20191021/
+MINILIBX=$(MINILIBX_DIR)/libmlx.a
+
+all: $(NAME)
+
+#$(NAME): $(OBJS)
+#	$(CC) -o $(NAME) $(OBJS)
+
+$(NAME): $(OBJS) $(MINILIBX)
+	$(CC) $(OBJS) -L$(MINILIBX_DIR) -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+#	$(CC) $(OBJS) -Lmlx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+
+
+#$(OBJS):
+%.o: %.c
+	$(CC) $(CFLAGS) -I$(MINILIBX_DIR) -c $< -o $@
+#	$(CC) -Wall -Wextra -Werror -Imlx -c $< -o $@
+
+$(MINILIBX):
+	$(MAKE) -C $(MINILIBX_DIR)
+
+.PHONY: clean fclean re
+
+clean:
+	$(RM) $(OBJS)
+	$(MAKE) -C $(MINILIBX_DIR) clean
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
---
+++ src/main.c
@@ -0,0 +1,53 @@
+
+
+#include "mlx.h"
+
+#define WINDOW_X 700
+#define WINDOW_Y 500
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, WINDOW_X, WINDOW_Y, "Hello world!");
+	img.img = mlx_new_image(mlx, WINDOW_X, WINDOW_Y);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+		&img.endian);
+	for (int j = 0; j < WINDOW_X; j += 100)
+	{
+		for (int i = 0; i < WINDOW_Y; i++)
+		{
+			my_mlx_pixel_put(&img, j, i, 0x00FF0000);
+		}
+	}
+	for (int j = 0; j < WINDOW_Y; j += 100)
+	{
+		for (int i = 0; i < WINDOW_X; i++)
+		{
+			my_mlx_pixel_put(&img, i, j, 0x0000FF00);
+		}
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
