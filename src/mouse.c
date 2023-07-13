aa293a7f6f2b58d12ed6ee97d2efdcc1f40a8673
Susumu Yagi <susumu.yagi@gmail.com>
2023-07-13 15:50:39 +0900

    refactored mouse.c

--- src/mouse.c
+++ src/mouse.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:10:59 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/07/13 15:28:11 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/07/13 15:49:45 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,56 @@ int	mouse_move(int x, int y, t_vars *vars)
 	return (0);
 }
 
+void	scroll_up(int x2, int y2, t_vars *vars)
+{
+	if (vars->keycode == KEY_R)
+	{
+		vars->c[0] += 0.005;
+		if (vars->c[0] > 2.0)
+			vars->c[0] -= 4.0;
+		drow_img(vars);
+	}
+	else if (vars->keycode == KEY_I)
+	{
+		vars->c[1] += 0.005;
+		if (vars->c[1] > 2.0)
+			vars->c[1] -= 4.0;
+		drow_img(vars);
+	}
+	else
+	{
+		vars->scale *= ZOOM_RATE;
+		vars->dx = (vars->dx + x2) * ZOOM_RATE - x2;
+		vars->dy = (vars->dy + y2) * ZOOM_RATE - y2;
+		drow_img(vars);
+	}
+}
+
+void	scroll_down(int x2, int y2, t_vars *vars)
+{
+	if (vars->keycode == KEY_R)
+	{
+		vars->c[0] -= 0.005;
+		if (vars->c[0] < -2.0)
+			vars->c[0] += 4.0;
+		drow_img(vars);
+	}
+	else if (vars->keycode == KEY_I)
+	{
+		vars->c[1] -= 0.005;
+		if (vars->c[1] < -2.0)
+			vars->c[1] += 4.0;
+		drow_img(vars);
+	}
+	else
+	{
+		vars->scale /= ZOOM_RATE;
+		vars->dx = (vars->dx + x2) / ZOOM_RATE - x2;
+		vars->dy = (vars->dy + y2) / ZOOM_RATE - y2;
+		drow_img(vars);
+	}
+}
+
 int	mouse_down(int key, int x, int y, t_vars *vars)
 {
 	int	x2;
@@ -30,50 +80,10 @@ int	mouse_down(int key, int x, int y, t_vars *vars)
 
 	x2 = x - vars->win_w / 2;
 	y2 = y - vars->win_h / 2;
-	if (key == SCROLL_UP && vars->keycode == KEY_R)
-	{
-		vars->c[0] += 0.005;
-		if (vars->c[0] > 2.0)
-			vars->c[0] -= 4.0;
-		drow_img(vars);
-	}
-	else if (key == SCROLL_UP && vars->keycode == KEY_I)
-	{
-		vars->c[1] += 0.005;
-		if (vars->c[1] > 2.0)
-			vars->c[1] -= 4.0;
-		drow_img(vars);
-	}
-	else if (key == SCROLL_UP)
-	{
-		// 拡大
-		vars->scale *= ZOOM_RATE;
-		vars->dx = (vars->dx + x2) * ZOOM_RATE - x2;
-		vars->dy = (vars->dy + y2) * ZOOM_RATE - y2;
-		drow_img(vars);
-	}
-	else if (key == SCROLL_DOWN && vars->keycode == KEY_R)
-	{
-		vars->c[0] -= 0.005;
-		if (vars->c[0] < -2.0)
-			vars->c[0] += 4.0;
-		drow_img(vars);
-	}
-	else if (key == SCROLL_DOWN && vars->keycode == KEY_I)
-	{
-		vars->c[1] -= 0.005;
-		if (vars->c[1] < -2.0)
-			vars->c[1] += 4.0;
-		drow_img(vars);
-	}
+	if (key == SCROLL_UP)
+		scroll_up(x2, y2, vars);
 	else if (key == SCROLL_DOWN)
-	{
-		// 縮小
-		vars->scale /= ZOOM_RATE;
-		vars->dx = (vars->dx + x2) / ZOOM_RATE - x2;
-		vars->dy = (vars->dy + y2) / ZOOM_RATE - y2;
-		drow_img(vars);
-	}
+		scroll_down(x2, y2, vars);
 	else if (key == MOUSE_LEFT)
 	{
 		vars->is_down = 1;
