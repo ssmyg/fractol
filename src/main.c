c970eb8674019dfe8481739d955855e2eac6982a
Susumu Yagi <susumu.yagi@gmail.com>
2023-07-13 12:45:15 +0900

    small modify message

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 12:11:44 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/07/13 12:38:09 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/07/13 12:43:45 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,19 @@ SYNOPSIS\n\
 	fractol model [param1] [param2]\n\
 \n\
 DESCRIPTION\n\
-	model: (mandelbrot | julia | sierpinski)\n\
-	param1: parameter1 for Julia set (real part)\n\
-	param2: parameter2 for Julia set (imaginary part)\n\
+	model	(mandelbrot | julia | sierpinski)\n\
+	param1	parameter1 for Julia set (real part)\n\
+	param2	parameter2 for Julia set (imaginary part)\n\
 \n\
 PARAMETER\n\
-	format: \"d.dddd\" (e.g. 1.2345)\n\
-	range: -2.0000 <= param <= 2.0000)\n\
+	format	\"d.dddd\" (e.g. 1.2345)\n\
+	range	-2.0000 <= param <= 2.0000)\n\
 \n\
 USAGE\n\
-	arrow keys :\tmove the view\n\
-	ESC :\t\tclose window\n\
-	s :\t\tshift color range\n\
-	0 :\t\tRestore the view to its initial state\n\
+	arrow	move the view\n\
+	ESC	close window\n\
+	s	shift color range\n\
+	0	Restore the view to its initial state\n\
 \n\
 ";
 }
