f6e71bc15fd27e8799040708dc416607dbccfb4b
Susumu Yagi <susumu.yagi@gmail.com>
2023-07-13 17:36:11 +0900

    shift color range by mouse

--- src/keyboard.c
+++ src/keyboard.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:08:16 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/07/13 16:52:43 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/07/13 17:34:21 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,16 @@
 #include "render.h"
 #include "window.h"
 
-void	key_z_r_i(int keycode, t_vars *vars)
+static void	key_zric(int keycode, t_vars *vars)
 {
 	if (keycode == KEY_Z)
 		vars->keycode = KEY_Z;
 	if (keycode == KEY_R)
-	{
-		if (vars->keycode == KEY_R)
-			vars->keycode = 0;
-		else
-			vars->keycode = KEY_R;
-	}
+		vars->keycode = KEY_R;
 	if (keycode == KEY_I)
-	{
-		if (vars->keycode == KEY_I)
-			vars->keycode = 0;
-		else
-			vars->keycode = KEY_I;
-	}
+		vars->keycode = KEY_I;
+	if (keycode == KEY_C)
+		vars->keycode = KEY_C;
 }
 
 void	key_arrow(int keycode, t_vars *vars)
@@ -55,11 +47,6 @@ int	key_hook(int keycode, t_vars *vars)
 {
 	if (keycode == KEY_ESC)
 		window_close(vars);
-	if (keycode == KEY_C)
-	{
-		vars->color = (vars->color + 10) % 360;
-		drow_img(vars);
-	}
 	if (keycode == KEY_0)
 	{
 		vars->scale = 1.0;
@@ -67,7 +54,7 @@ int	key_hook(int keycode, t_vars *vars)
 		vars->dy = 0.0;
 		drow_img(vars);
 	}
-	key_z_r_i(keycode, vars);
+	key_zric(keycode, vars);
 	key_arrow(keycode, vars);
 	return (0);
 }
--- src/mouse.c
+++ src/mouse.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:10:59 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/07/13 16:51:30 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/07/13 17:07:57 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,22 @@ void	scroll_up(int x2, int y2, t_vars *vars)
 		vars->c[0] += 0.005;
 		if (vars->c[0] > 2.0)
 			vars->c[0] -= 4.0;
-		drow_img(vars);
 	}
 	else if (vars->keycode == KEY_I)
 	{
 		vars->c[1] += 0.005;
 		if (vars->c[1] > 2.0)
 			vars->c[1] -= 4.0;
-		drow_img(vars);
 	}
+	else if (vars->keycode == KEY_C)
+		vars->color = (vars->color + 1) % 360;
 	else
 	{
 		vars->scale *= ZOOM_RATE;
 		vars->dx = (vars->dx + x2) * ZOOM_RATE - x2;
 		vars->dy = (vars->dy + y2) * ZOOM_RATE - y2;
-		drow_img(vars);
 	}
+	drow_img(vars);
 }
 
 void	scroll_down(int x2, int y2, t_vars *vars)
@@ -58,22 +58,26 @@ void	scroll_down(int x2, int y2, t_vars *vars)
 		vars->c[0] -= 0.005;
 		if (vars->c[0] < -2.0)
 			vars->c[0] += 4.0;
-		drow_img(vars);
 	}
 	else if (vars->keycode == KEY_I)
 	{
 		vars->c[1] -= 0.005;
 		if (vars->c[1] < -2.0)
 			vars->c[1] += 4.0;
-		drow_img(vars);
+	}
+	else if (vars->keycode == KEY_C)
+	{
+		vars->color -= 1;
+		if (vars->color < 0)
+			vars->color = 360;
 	}
 	else
 	{
 		vars->scale /= ZOOM_RATE;
 		vars->dx = (vars->dx + x2) / ZOOM_RATE - x2;
 		vars->dy = (vars->dy + y2) / ZOOM_RATE - y2;
-		drow_img(vars);
 	}
+	drow_img(vars);
 }
 
 int	mouse_down(int key, int x, int y, t_vars *vars)
