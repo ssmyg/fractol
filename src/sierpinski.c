afa94a1113935d8f5910ff1da2d0dac925b937e7
Susumu Yagi <susumu.yagi@gmail.com>
2023-07-19 18:18:43 +0900

    Coordinate transformation(sierpinske)

--- src/sierpinski.c
+++ src/sierpinski.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:16:40 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/07/19 15:44:41 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/07/19 18:18:23 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,16 @@ int	func(double x, double y, int depth, int max)
 	return (func(x, y, depth + 1, max));
 }
 
+// inverse([[1, 1/2], [0, sqrt(3)/2]])
+// = 1/3 [[3, -sqrt(3)], [0, 2 * sqrt(3)]]
 int	sierpinski(int w, int h, t_vars *vars)
 {
 	int		max;
 	double	c[2];
 
 	to_z(w, h, c, vars);
+	c[0] = c[0] - sqrt(3) * c[1] / 3.0;
+	c[1] = 2.0 * sqrt(3) / 3.0 * c[1];
 	max = log2(vars->scale) + 6;
 	return (func(c[0], c[1], 0, max));
 }
