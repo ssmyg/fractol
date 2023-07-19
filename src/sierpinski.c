2664578fa1fa14029bad86575d2b23c30d1ab79e
Susumu Yagi <susumu.yagi@gmail.com>
2023-07-19 15:46:12 +0900

    modify max depth of sierpinski

--- src/sierpinski.c
+++ src/sierpinski.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:16:40 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/07/13 16:16:24 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/07/19 15:44:41 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,6 @@ int	sierpinski(int w, int h, t_vars *vars)
 	double	c[2];
 
 	to_z(w, h, c, vars);
-	max = log10(vars->scale) * 3 + 8;
+	max = log2(vars->scale) + 6;
 	return (func(c[0], c[1], 0, max));
 }
